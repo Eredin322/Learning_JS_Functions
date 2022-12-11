96a9592c9028a7d9a91d0ddc116687ab543e8014
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-12-11 14:21:50 +0200

    Lecture #4

--- Lesson4/Lecture/js/script.js
+++ Lesson4/Lecture/js/script.js
@@ -2,5 +2,58 @@ function sum(a, b) {
     return a + b;
 }
 
+function multi(a, b) {
+    return a * b;
+}
+
 let a = sum(4, 5);
 console.log(a);
+
+document.querySelector('.b-1').addEventListener('click', () => {
+    let res = sum(23, 45);
+    console.log(res);
+});
+
+// Запуск двух функций
+document.querySelector('.b-2').addEventListener('click', () => {
+    let res = sum(23, 45);
+    console.log(res);
+    let res2 = multi(23, 45);
+    console.log(res2);
+});
+
+//callback
+const ar1 = [4, 5, 6, 7, 8];
+function pow2(a) {
+    return a ** 2;
+}
+const res = ar1.map(pow2);
+console.log(res);
+
+// filter
+
+const res6 = ar1.filter(function (item, index) {
+    if (index % 2 === 0) return true;
+})
+console.log(res6);
+// Пишем на стрелочной функции
+const res7 = ar1.filter((item, index) => index % 2 === 0)
+console.log(res7);
+
+//arrow & this
+document.querySelector('.b-3').addEventListener('click', function () {
+    console.log(this);
+})
+
+document.querySelector('.b-3').addEventListener('click', () => {
+    console.log(this);
+})
+
+// arrow аргументы
+
+const f1 = (...args) => {
+    // console.log(arguments);
+    console.log('work ne wolk');
+    return args.reduce((accum, item) => accum + item)
+}
+f1();
